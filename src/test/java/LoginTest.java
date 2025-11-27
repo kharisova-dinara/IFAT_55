a7c4fa3c4c59ee99124b274c996db1c63c600377
Dinara Kharisova <nurutdinovadinara@gmail.com>
2025-11-27 17:47:08 +0400

    Lesson4

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -4,7 +4,7 @@ import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
 public class LoginTest extends BaseTest {
-    @Test
+    @Test(description = "Проверка корректного логина", priority = 1)
     public void checkIncorrectLogin() {
         loginPage.open();
         loginPage.login("locked_out_user", "secret_sauce");
@@ -12,7 +12,7 @@ public class LoginTest extends BaseTest {
         assertEquals(loginPage.errorMessageText(), "Epic sadface: Sorry, this user has been locked out.");
     }
 
-    @Test
+    @Test(priority = 2, enabled = true, invocationCount = 7, alwaysRun = true)
     public void checkCorrectLogin() {
         loginPage.open();
         loginPage.login("standard_user", "secret_sauce");
